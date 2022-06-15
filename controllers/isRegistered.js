fc398017c72cbe6fbf23be8aaea7b55b2f6f94ec
unknown <hassanmohammad922@gmail.com>
2022-06-15 03:20:10 +0300

    Added last name to registration api

--- controllers/isRegistered.js
+++ controllers/isRegistered.js
@@ -1,7 +1,7 @@
 const handleIsRegistered = (req, res, db) => {
   const { id } = req.params;
   db("registration")
-    .select("registered", "first_name")
+    .select("registered", "first_name","last_name")
     .where({ std_id: id.toUpperCase() })
     .then((row) => {
       res.json(row);
