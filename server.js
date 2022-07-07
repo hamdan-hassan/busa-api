a7bc0272031d23eca1dc28d45bc85600ff5dabd2
hamdan <hassanmohammad922@gmail.com>
2022-07-07 03:50:09 -0700

    Fixed request entity to large error duplicate code

--- server.js
+++ server.js
@@ -58,7 +58,6 @@ const db = knex({
 
 const app = express();
 app.use(bodyParser.json({ limit: "50mb" }));
-app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(
   bodyParser.urlencoded({
     limit: "50mb",
